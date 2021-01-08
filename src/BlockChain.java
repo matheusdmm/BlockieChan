95282848c6093c9c28935481d6caddc2e7fb1832
Matheus <mfonse0@gmail.com>
2021-01-08 14:04:28 -0300

    Init

---
+++ src/BlockChain.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import com.google.gson.GsonBuilder;
+
+public class BlockChain {
+
+    public static ArrayList<Block> blockchain = new ArrayList<Block>();
+
+    public static void main(String[] args) {
+
+        blockchain.add(new Block("Hi im the first block", "0"));
+        blockchain.add(new Block("Yo im the second block",blockchain.get(blockchain.size()-1).hash));
+        blockchain.add(new Block("Hey im the third block",blockchain.get(blockchain.size()-1).hash));
+
+        String blockchainJson = new GsonBuilder().setPrettyPrinting().create().toJson(blockchain);
+        System.out.println(blockchainJson);
+
+    }
+}
